baaa649a28eb9b56013baea6fd0e54ed83d37fed
starx <swe_r3p0x9@mrnepal.com>
2026-01-13 14:26:56 +0000

    move inside folder

--- exercises/exclusive_items/exclusive_items.php
+++ exercises/exclusive_items/exclusive_items.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once "test_runner.php";
+require_once "../test_runner.php";
 
 class ExclusiveItems {
     public function sol1_bruteforce(array $a, array $b) {
