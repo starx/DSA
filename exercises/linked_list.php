c391712b2ae6fa2bc24b28beba5a956545fa4834
starx <swe_r3p0x9@mrnepal.com>
2026-01-21 12:11:44 +0000

    a helper function to turn list to array

--- exercises/linked_list.php
+++ exercises/linked_list.php
@@ -6,3 +6,13 @@ class Node {
         public ?Node $next = null
     ) {}
 }
+
+function list_to_value_array(?Node $head = null): array {
+    if(!$head) return [];
+    $vArr = [];
+    while($head) {
+        $vArr[] = $head->value;
+        $head = $head->next;
+    }
+    return $vArr;
+} 
