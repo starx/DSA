4a1ce1eefd22fb42f747ae5ff51ce8a4dc8afefc
starx <swe_r3p0x9@mrnepal.com>
2026-01-29 07:52:35 +0000

    add helper function

--- exercises/binary_tree.php
+++ exercises/binary_tree.php
@@ -9,3 +9,42 @@ class Node {
 
     }
 }
+
+public function tree_to_value_array_depth_first(?Node $root): array {
+    $values = [];
+    if ($root !== null) {
+        $values[] = $root->value;
+
+        if ($root->left !== null) {
+            $values = array_merge($values, tree_to_value_array_depth_first($root->left));
+        }
+        if ($root->right !== null) {
+            $values = array_merge($values, tree_to_value_array_depth_first($root->right));
+        }
+    }
+    return $values;
+}
+
+function tree_to_value_array_breadth_first(?Node $root): array {
+    $values = [];
+    $queue = new SplQueue();
+
+    if ($root !== null) {
+        $queue->enqueue($root);
+    }
+
+    while (!$queue->isEmpty()) {
+        /** @var Node $currNode */
+        $currNode = $queue->dequeue();
+        $values[] = $currNode->value;
+
+        if ($currNode->left !== null) {
+            $queue->enqueue($currNode->left);
+        }
+
+        if ($currNode->right !== null) {
+            $queue->enqueue($currNode->right);
+        }
+    }
+
+    return $values;}
