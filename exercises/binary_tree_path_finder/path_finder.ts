f95e496045255fe51f2cdd67d35d55c67ce680f6
starx <swe_r3p0x9@mrnepal.com>
2026-02-08 15:15:36 +0000

    added bfs search

--- exercises/binary_tree_path_finder/path_finder.ts
+++ exercises/binary_tree_path_finder/path_finder.ts
@@ -7,20 +7,20 @@ class PathFinder<T> {
 
     }
 
-    sol1_depth_first(root: Node<T>|null, target: T): T[]|null {
+    sol1_depth_first_recursive(root: Node<T>|null, target: T): T[]|null {
 
         if(!root) return null;
         if(root.value === target) {
             return [root.value]
         }
         if(root.left) {
-            const leftResult = this.sol1_depth_first(root.left, target);
+            const leftResult = this.sol1_depth_first_recursive(root.left, target);
             if(leftResult) {
                 return [root.value, ...leftResult];
             }
         }
         if(root.right) {
-            const rightResult = this.sol1_depth_first(root.right, target);
+            const rightResult = this.sol1_depth_first_recursive(root.right, target);
             if(rightResult) {
                 return [root.value, ...rightResult];
             }
@@ -29,11 +29,54 @@ class PathFinder<T> {
         return null;
 
     }
+
+    sol2_breadth_first(root: Node<T>|null, target: T): T[]|null {
+
+        if(!root) return null;
+
+        let queue: Node<T>[] = [];
+        let parentMap = new Map<Node<T>, Node<T>|null>();
+        parentMap.set(root, null);
+
+        queue.push(root);
+        let i = 0;
+
+        while(i < queue.length) {
+            const curNode = queue[i];
+
+            if (curNode.value === target) {
+                let path: T[] = [];
+                let loopNode: Node<T>|null = curNode;
+                while(loopNode) {
+                    path.push(loopNode.value);
+                    loopNode = parentMap.get(loopNode) ?? null
+                }
+
+                return path.reverse();
+            }
+
+            if(curNode.left) {
+                parentMap.set(curNode.left, curNode);
+                queue.push(curNode.left);
+            }
+            if(curNode.right) {
+                parentMap.set(curNode.right, curNode);
+                queue.push(curNode.right);
+            }
+
+            i++;
+        }
+
+
+
+        return null;
+
+    }
 }
 
 run_test(
     new PathFinder(),
-    'sol1_depth_first',
+    'sol2_breadth_first',
     test_cases,
     false
 );
