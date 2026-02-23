d0eea07581e3349599a058ae4f93f57da63d8d3a
starx <swe_r3p0x9@mrnepal.com>
2026-02-23 21:28:51 +0000

    further cleanup

--- exercises/l2_no_of_disc_intersections/no_of_disc_intersections.py
+++ exercises/l2_no_of_disc_intersections/no_of_disc_intersections.py
@@ -14,17 +14,17 @@ class NoOfDiscIntersections:
         count = 0
         for i in range(N):
             for j in range(i+1, N):
-                circle1 = ranges[i] # l1, r1
-                circle2 = ranges[j] # l2, r2
+                L1, R1 = ranges[i] # cirle 1: l1, r1
+                L2, R2 = ranges[j] # circle 2: l2, r2
                 # If the circles do not intersect, then one of the following must be true:
                 # - The right edge of circle1 is to the left of the left edge of circle2: r1 < l2
                 # - The left edge of circle1 is to the right of the right edge of circle2: l1 > r2
-                if circle1[1] < circle2[0] or circle1[0] > circle2[1]:
+                if R1 < L2 or L1 > R2:
                     continue
-                else:
-                    count += 1
-                    if count > 10000000:
-                        return -1
+
+                count += 1
+                if count > 10000000:
+                    return -1
 
 
         return count
